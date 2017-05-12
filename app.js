9ee3a4e94e11535630429a2b462664d73112f105
Joseph Ou <j0u@outlook.com>
2017-05-12 05:22:29 -0700

    retrofit backend to suit multi-address

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var bodyParser = require('body-parser');
 var app = express();
 var path = require('path');
 var haversine = require('haversine')
-
+var request = require('request');
 
 
 //bodyparser parses our json file for post and get
@@ -27,11 +27,8 @@ coordinateAddress = [];
 app.post('/addrToBackend/', function(req, res){
     
     var search = req.body.search;
-    var distanceOptions = req.body.distanceOptions;
-    var addr1 = req.body.data.addr1;
-    var addr2 = req.body.data.addr2;
-    var data = req.body.data;
-    var addr = [addr1, addr2];
+    var distanceOptions = parseFloat(req.body.distanceOptions);
+    var data = JSON.parse(req.body.data.addr);
 
     console.log([search,distanceOptions,data]);
 
@@ -40,10 +37,11 @@ app.post('/addrToBackend/', function(req, res){
     //rezero the number of addresses being counted
     runSearchCount = 0;
         
-//        parseCoord(addr);
+    parseCoord(data,search,distanceOptions);
     res.end();
     
 });
+
 
 app.post('/addrToFrontend/', function(req, res){
     res.type('text/plain');
@@ -59,25 +57,35 @@ app.listen(8081, function () {
 })
 
 var googleMapsClient = require('@google/maps').createClient({
-  key: 'AIzaSyBobCvR6fD_t4KwHfSL-EjprfmuYk5QJrw'
+  key: 'AIzaSyBwQXLLkxWCyoOUfwMRVkfsk6lECoFmNFk'
 });
 
 
-//this function gets distances of each element from 
+//this function gets distances of each element from each address
 function runSort(){
     console.log("in runsort()");
     var distancedResponse = [];
-    
+
+    // for(var i = 0; i < global.coordinateAddress.length; i++){
+        console.log(global.coordinateAddress.length);
+        console.log(global.coordinateAddress);
+    // }
+
+
+
+
+
+
     //uses Haversine formula by niix to calculate distance from each input address to each google address
 //    haversine(start, end, {unit: 'mile'});
-        var addr1 = {
-            latitude: global.coordinateAddress[0][0],
-            longitude: global.coordinateAddress[0][1]
-        }
-        var addr2 = {
-            latitude: global.coordinateAddress[1][0],
-            longitude: global.coordinateAddress[1][1]
-        }
+    var addr1 = {
+        latitude: global.coordinateAddress[0][0],
+        longitude: global.coordinateAddress[0][1]
+    }
+    var addr2 = {
+        latitude: global.coordinateAddress[1][0],
+        longitude: global.coordinateAddress[1][1]
+    }
         
     for(var i = 0; i < global.searchResponse.length; i++){
         var addr = {
@@ -97,6 +105,7 @@ function runSort(){
                 distance: combinedDistance
             });
         }
+        // console.log(distancedResponse);
     }
     
     console.log("Performing sorting by distance");
@@ -112,24 +121,28 @@ function runSort(){
 
 
 function pushResponseToArray(results){
-    for(var i = 0; i< results.length; i++){
+    console.log("in pushResponseToArray");
+    for(var i = 0; i < results.length; i++){
         var elementName = results[i].name;
         var textAddress = results[i].vicinity;
         var geoAddress = results[i].geometry.location;
-        global.searchResponse.push({name: elementName, address: textAddress, coordinates: geoAddress});
+        var placeData = results[i].placeID;
+        var rating = results[i].rating;
+        var price = results[i].price_level;
+        global.searchResponse.push({name: elementName, address: textAddress, coordinates: geoAddress,
+             placeID: placeData, rating: rating, price: price});
     }
 }
 
-
-//This function is not needed. It prunes out locations that are not 
 function removeNonDupes(){
+    console.log("removing all dupes");
     var prunedResponse = [];
     
     
     //sort the two concatenated arrays
     global.searchResponse.sort(function(a,b){
-        if (a.name < b.name)return -1;
-        if (a.name > b.name)return 1;
+        if (a.address < b.address)return -1;
+        if (a.address > b.address)return 1;
     });
     
     for (var i = 1; i < global.searchResponse.length; i++){
@@ -142,123 +155,111 @@ function removeNonDupes(){
     }
 
     global.searchResponse = prunedResponse;
-    
-    //calls runSort(), which sorts by distance and returns the final data we want
-    runSort();
+    console.log("removed all dupes");
+    for(var i = 0; i < global.searchResponse.length; i++){
+        console.log(global.searchResponse[i].name);
+    }
 }
 
 //checks for when the search has been completed, and then runs removeNonDupes();
-function checkAndExecuteSort(){
+function checkAndExecuteSort(addrCount){
     runSearchCount++;
     console.log("in checkandexecutesort");
-    if (runSearchCount > 1){
+    if (runSearchCount == addrCount){
         runSearchCount = 0;
+        console.log("final iteration, running removeNonDupes");
         removeNonDupes();
+    }
+}
+
+//runs google maps api's geocode function to grab the coordinates
+function runSearch(addr, search, distanceOptions, addrCount){
+    //note: depending on keyword, the returned results may change slightly.
+    //also, due to the 60 result limit, if more than 60 hits exist
+    //possible candidates on the list will be truncated and the end result
+    //may differ because of this.
+    console.log("in runSearch()");
+
+    googleMapsClient.placesNearby(
+        {
+            location: addr,
+            radius: distanceOptions,
+            keyword: search
+            //rankby: 'distance' should theoretically get better results, but some testing reveals
+            //using radius instead appears to give many more valid hits
+        },
+        function(err, response) {
+            if (!err) {
+                console.log("success in retrieving data from googlemapsclient");
+                
+                pushResponseToArray(response.json.results);
+                if (response.json.next_page_token != null){
+                    // console.log("starting recursesearch");
+                    //must wait for google's servers to update its pagetokens
+                    //2 sec appears to be the threshold. 1.5sec wait results in inconsistent
+                    //results
+                    // setTimeout(function(){recurseSearch(addr, response.json.next_page_token);}, 2000);
+                }
+                checkAndExecuteSort(addrCount);
+                
+            }
+        }
+    )
+}
+
+function parseCoord(data, search, distanceOptions){
+    
+    console.log("passing in data: " + data);
+    // Geocode the two addresses.
+    for (var i = 0; i < data.length; i++){
+        console.log("data.length =" + data.length);
+        googleMapsClient.geocode(
+            {
+            address: data[i]
+            }, function(err, response) {
+            if (!err) {
+                var addrCoordTemp = response.json.results[0].geometry.location;
+                // console.log("processing addr coordinates");
+                var addrCoord = [addrCoordTemp.lat, addrCoordTemp.lng];
+                global.coordinateAddress.push(addrCoord);
+                //nesting placesNearby inside geocode to effectively render this
+                //to run sequentially as opposed to asynchronously.
+                //this is done to guarantee placesNearby has the geocode
+                //it needs-- else, addr1Coord may be undefined at run time
+
+
+                runSearch(addrCoord, search, distanceOptions, data.length);
+            } else console.log("FAILURE to retrieve google maps data in parseCoord()");
+        });
     }
 }
 
 //google places api allows up to 60 results to be returned, with 20 results per page
 //this function gets the other 40 results.
 //also will work fine if google ever decides to be generous and give more than 60 results.
-function recurseSearch(addr, ptoken){
-    console.log("in recurseSearch");
-    googleMapsClient.placesNearby(
-        {   //16093.4m == 10.000mi
-            location: addr,
-            radius: 16093.4,
-            keyword: 'real_estate_agency',
-            pagetoken: ptoken
-        },
-        function(err, response) {
-            console.log("in gmaps client function");
-            if (!err) {
-                pushResponseToArray(response.json.results);
-                if (response.json.next_page_token != null){
-                    //must wait for google's servers to update its pagetokens
-                    //2 sec appears to be the threshold. 1.5sec wait results in inconsistent
-                    //results
-                    setTimeout(function(){recurseSearch(addr, response.json.next_page_token);}, 2000);
-                }else {
-                    checkAndExecuteSort();
-                }
+// function recurseSearch(addr, ptoken){
+//     console.log("in recurseSearch");
+//     googleMapsClient.placesNearby(
+//         {   //16093.4m == 10.000mi
+//             location: addr,
+//             radius: 16093.4,
+//             keyword: 'real_estate_agency',
+//             pagetoken: ptoken
+//         },
+//         function(err, response) {
+//             console.log("in gmaps client function");
+//             if (!err) {
+//                 pushResponseToArray(response.json.results);
+//                 if (response.json.next_page_token != null){
+//                     //must wait for google's servers to update its pagetokens
+//                     //2 sec appears to be the threshold. 1.5sec wait results in inconsistent
+//                     //results
+//                     setTimeout(function(){recurseSearch(addr, response.json.next_page_token);}, 2000);
+//                 }else {
+//                     checkAndExecuteSort();
+//                 }
                 
-            } else checkAndExecuteSort();
-        }
-    )
-}
-
-//runs google maps api's geocode function to grab the coordinates
-function runSearch(addr){
-    //note: depending on keyword, the returned results may change slightly.
-    //also, due to the 60 result limit, if more than 60 hits exist
-    //possible candidates on the list will be truncated and the end result
-    //may differ because of this.
-    
-    //realtor and realty may provide differing results.
-    //as the requested requirement in the assignment prompt was "real estate agency"
-    //the only keyword will thus be google's official keyword, "real_estate_agency"
-    googleMapsClient.placesNearby(
-        {
-            location: addr,
-            radius: 16093.4,
-            keyword: 'real_estate_agency'
-            //rankby: 'distance' should theoretically get better results, but some testing reveals
-            //using radius instead appears to give many more valid hits
-        },
-        function(err, response) {
-            if (!err) {
-                console.log("in runSearch");
-                pushResponseToArray(response.json.results);
-                if (response.json.next_page_token != null){
-                    console.log("starting recursesearch");
-                    //must wait for google's servers to update its pagetokens
-                    //2 sec appears to be the threshold. 1.5sec wait results in inconsistent
-                    //results
-                    setTimeout(function(){recurseSearch(addr, response.json.next_page_token);}, 2000);
-                }else {
-                    checkAndExecuteSort();
-                }
-                
-            } else return;
-        }
-    )
-}
-
-function parseCoord(addr){
-    
-    console.log("passing in data: " + addr);
-    // Geocode the two addresses.
-    googleMapsClient.geocode(
-        {
-        address: addr[0]
-        }, function(err, response) {
-        if (!err) {
-            var addr1coordTemp = response.json.results[0].geometry.location;
-            var addr1coord = [addr1coordTemp.lat, addr1coordTemp.lng];
-            console.log("processing addr1 coordinates");
-            global.coordinateAddress[0] = addr1coord;
-//            console.log(addr1coord);
-            
-            //nesting placesNearby inside geocode to effectively render this
-            //to run sequentially as opposed to asynchronously.
-            //this is done to guarantee placesNearby has the geocode
-            //it needs-- else, addr1Coord may be undefined at run time
-            runSearch(addr1coord);
-        } else return;
-    });
-    
-    googleMapsClient.geocode(
-        {
-        address: addr[1]
-        }, function(err, response) {
-        if (!err) {
-            var addr2coordTemp = response.json.results[0].geometry.location;
-            addr2coord = [addr2coordTemp.lat, addr2coordTemp.lng];
-            console.log("processing addr2 coordinates");
-            global.coordinateAddress[1] = addr2coord;
-//            console.log(addr2coord);
-            
-            runSearch(addr2coord);
-        } else return;
-    });
-}
+//             } else checkAndExecuteSort();
+//         }
+//     )
+// }
